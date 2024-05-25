147e58ea2631f344c039c650800415cc3a1d4a64
LeoIsidro <leonardo.isidro@utec.edu.pe>
2024-05-25 09:23:31 -0500

    Avance

--- main.js
+++ main.js
@@ -4,10 +4,7 @@ const qrcode = require('qrcode-terminal');
 // Cliente de WhatsApp
 
 const client = new Client({
-    authStrategy: new LocalAuth({
-        clientId: "client-one",
-        dataPath: './session-data' // Ruta personalizada para almacenar los datos de la sesión
-    }),
+
     webVersionCache: {
       type: "remote",
       remotePath:
@@ -27,14 +24,90 @@ client.on('disconnected', (reason) => {
     console.log('Client was logged out', reason);
 });
 
+async function getMensaje() {
+    return new Promise(resolve => {
+        client.once('message', msg => {
+            if (msg.from === usuario ) {
+                resolve(msg.body);
+            }
+        });
+    });
+}
+
+var usuario='51970579052@c.us';
+
+
 // Funciones de obtencion de la base de datos
 
-const getInventario = () => {
+async function getInventario () {
     return 'Inventario';
 }
 
-const insertarVenta = (producto) => {
-    return 'Venta';
+
+async function insertarVenta () {
+
+    client.sendMessage(usuario, 'Ingrese la cantidad de productos vendidos');
+
+    // Se almacena la cantidad de productos vendidos  
+    var cantidad = await getMensaje();
+    console.log(cantidad);
+
+    while (cantidad>0) {
+        client.sendMessage(usuario, 'Ingrese el nombre del producto');
+        var producto = await getMensaje();
+        console.log(producto);
+        // Se inserta en la base de datos la venta
+
+        //
+        cantidad--;
+    }
+    client.sendMessage(usuario, 'Venta registrada');  
+}
+
+async function registrarInventario  ()  {
+    client.sendMessage(usuario, 'Ingrese el nombre del producto');
+    var producto = await getMensaje();
+    console.log(producto);
+    client.sendMessage(usuario, 'Ingrese la cantidad del producto');
+    var cantidad = await getMensaje();
+    console.log(cantidad);
+    client.sendMessage(usuario, 'Ingrese el precio del producto');
+    var precio = await getMensaje();
+    console.log(precio);
+    // Se inserta en la base de datos el producto
+
+    client.sendMessage(usuario, 'Inventario registrado');
+}
+
+
+// Funciones de respuesta
+
+async function mostrarOperaciones () {
+    client.sendMessage(usuario, 'Bienvenido Usuario, que operacion deseas realizar\n 1. Consultar Inventario\n2. Registrar nueva venta \n3. Registrar Inventario\n4. Salir');
+    const opcion = await getMensaje();
+        switch (opcion) {
+            case '1':
+                client.sendMessage(usuario, 'Aquí está el inventario...');
+                await getInventario();
+                break;
+            case '2':
+                client.sendMessage(usuario, 'Registrando nueva venta...');
+                await insertarVenta();
+                mostrarOperaciones();
+                break;
+            case '3':
+                client.sendMessage(usuario, 'Registrando inventario...');
+                await registrarInventario();
+                break;
+            case '4':
+                client.sendMessage(usuario, 'Saliendo...');
+                Inicio();
+                break;
+            default:
+                client.sendMessage(usuario, 'Opción no válida, intente nuevamente');
+                mostrarOperaciones();
+                break;
+          } 
 }
 
 
@@ -42,52 +115,15 @@ const insertarVenta = (producto) => {
 
 // Escuchando mensajes
 
-client.on('message', msg => {
-    console.log('Message received', msg.body);
-    console.log('From', msg.from); 
-    if (msg.from ==='51940425961@c.us' && msg.type === 'chat') {
-      if (msg.body === 'Hola') {
-        client.sendMessage(msg.from, 'Bienvenido matias, que operacion deseas realizar\n 1. Consultar Inventario\n2. Registrar nueva venta \n3. Registrar Inventario\n4. Consultar movimientos\n5. Salir');
-      }
-      else if (!isNaN(msg.body)) {
-        switch (msg.body) {
-          case '1':
-              client.sendMessage(msg.from, 'Aquí está el inventario...');
-              client.sendMessage(msg.from, getInventario());
-              break;
-          case '2':
-              client.sendMessage(msg.from, 'Registrando nueva venta...');
-              client.sendMessage(msg.from, 'Ingrese la cantidad de productos vendidos');
-              // Aquí se debería esperar a que el usuario ingrese la cantidad de productos vendidos
-              // Se almacena la cantidad de productos vendidos
-              var cantidad = msg.body;
-              while (cantidad) {
-                client.sendMessage(msg.from, 'Ingrese el nombre del producto vendido');
-                var producto = msg.body;
-                insertarVenta(producto);
-                cantidad--;
-              }
-              client.sendMessage(msg.from, 'Venta registrada');              
-              break;
-          case '3':
-              client.sendMessage(msg.from, 'Registrando inventario...');
-              client.sendMessage(msg.from, 'Inventario registrado');
-              break;
-          case '4':
-              client.sendMessage(msg.from, 'Aquí están los movimientos...');
-              break;
-          case '5':
-              client.sendMessage(msg.from, 'Saliendo...');
-              break;
-          default:
-              client.sendMessage(msg.from, 'Opción no válida, intente nuevamente');
-              break;
-        }
-      }
+async function Inicio() {
+    var msg = await getMensaje();
+    console.log(msg);
+    while( msg !== 'Hola') {
+         msg = await getMensaje();
     }
-});
-
-client.sendMessage('519404259', 'Hola');
+    
+    mostrarOperaciones();
+}
 
 
 
@@ -97,3 +133,5 @@ client.on('qr', qr => {
 });
 
 client.initialize();
+
+Inicio();
